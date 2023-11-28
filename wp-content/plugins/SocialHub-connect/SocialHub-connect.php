041f2116bc4f6177299cfdcb745dc7e3dccd5d58
Mouadh Aouiti <a.mouadh@emerya.fr>
2023-11-28 10:05:12 +0100

    first commit

---
+++ wp-content/plugins/SocialHub-connect/SocialHub-connect.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       SocialHub Connect
+ * Plugin URI:        
+ * Description:       SocialHub Connect est un plugin WordPress qui facilite la gestion et l'intégration des interactions sociales directement depuis votre site web. Il permet aux utilisateurs de connecter et de synchroniser leurs profils de médias sociaux (Facebook, Twitter, Instagram, LinkedIn, etc.) avec leur compte WordPress.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mouadh Aouiti
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
